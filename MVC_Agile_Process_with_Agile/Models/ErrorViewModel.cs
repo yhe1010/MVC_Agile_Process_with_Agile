e9f5c9ca2b68e0b3f6f0ad89ed9f1345ba626f5f
Yanhua He <yhe101@my.centennialcollege.ca>
2025-03-16 00:44:44 -0400

    Add project files.

---
+++ MVC_Agile_Process_with_Agile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Agile_Process_with_Agile.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
